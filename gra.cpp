f4b2fa4ac815df0c5c0f508ddb635ba177e1d220
Szymek8828 <106557435+Szymek8828@users.noreply.github.com>
2023-01-24 19:36:42 +0100

    Add files via upload

--- gra.cpp
+++ gra.cpp
@@ -10,44 +10,51 @@ using namespace std;
 		int sterowanie[4][2] = {};
 		
 		cout<<"Przypisz przycisk poruszania sie w lewo: "<<endl;
-		if(kbhit()){
-			sterowanie[0][0]=getch();
-			sterowanie[0][1]=getch();
-		}
-		else{
-			sterowanie[0][0]=getch();
-		}
 		
+		int left[2]={};
+		left[0]=getch();
+		if (kbhit()){
+			left[1]=getch();
+
+			}
+		sterowanie[0][0]=left[0];
+		sterowanie[0][1]=left[1];
+
 		
-		cout<<"Przypisz przycisk poruszania sie w prawo "<<endl;	
-		if(kbhit()){
-			sterowanie[1][0]=getch();
-			sterowanie[1][1]=getch();
-		}
-		else{
-			sterowanie[1][0]=getch();
-		}
+		cout<<"Przypisz przycisk poruszania sie w prawo "<<endl;
+
+		int right[2]={};
+		right[0]=getch();
+		if (kbhit()){
+			right[1]=getch();
+
+			}
+		sterowanie[1][0]=right[0];
+		sterowanie[1][1]=right[1];
+
+		cout<<"Przypisz przycisk poruszania sie w gore"<<endl;	
 		
-		
-		cout<<"Przypisz przycisk poruszania sie w gore "<<endl;
-		if(kbhit()){
-			sterowanie[2][0]=getch();
-			sterowanie[2][1]=getch();
-		}
-		else{
-			sterowanie[2][0]=getch();
-		}
-		
-			
+		int up[2]={};
+		up[0]=getch();
+		if (kbhit()){
+			up[1]=getch();
+
+			}
+		sterowanie[2][0]=up[0];
+		sterowanie[2][1]=up[1];
+					
 		cout<<"Przypisz przycisk poruszania sie w dol "<<endl;
-			if(kbhit()){
-				sterowanie[3][0]=getch();
-				sterowanie[3][1]=getch();
+
+		int down[2]={};
+		down[0]=getch();
+		if (kbhit()){
+			down[1]=getch();
+
 			}
-			else{
-				sterowanie[3][0]=getch();
-			}
-			
+		sterowanie[3][0]=down[0];
+		sterowanie[3][1]=down[1];
+		
+					
 		
 		cout<<sterowanie[0][0]<<" "<<sterowanie[0][1]<<endl;
 		cout<<sterowanie[1][0]<<" "<<sterowanie[1][1]<<endl;
@@ -67,60 +74,78 @@ using namespace std;
 		int SnakeX=1+rand()%(x-2);
 		int SnakeY=1+rand()%(y-2);
 		
+		
+
 		for(int i=0;i<y;i++){
 			for(int j=0;j<x;j++){
-				plansza[i][j]=' ';
+				if((j==0||j==y-1)&&(i!=0&&i!=y-1)){
+					plansza[j][i]=186;
+				}
+				else if((j!=0&&j!=x-1)&&(i==0||i==y-1)){
+				plansza[j][i]=205;
+				}
+	
+			else{
+				plansza[j][i]=' ';
 			}
-			
 		}
+		plansza[SnakeX][SnakeY]=snake;
+		plansza[0][0]=201;
+		plansza[x-1][0]=187;
+		plansza[x-1][y-1]=188;
+		plansza[0][y-1]=200;
 		
 		for(int i=0;i<y;i++){
+			for(int j=0;j<x;j++){
+				if(plansza[j][i]=='*'){
+					cout<<char(2);
+				}
+				else{
+					cout<<plansza[j][i];
+				}
+			}
 			cout<<endl;
-			for(int j=0;j<x;j++){
-				plansza[0][j]='*';
-				plansza[x-1][j]='*';
-				plansza[i][0]='*';
-				plansza[i][y-1]='*';
-				plansza[SnakeX][SnakeY]=snake;
-				cout<<plansza[i][j];
-			}
-		}
-		
-		int prawo, gora, dol;
-		int lewo[1][2] = {};
-		
-		if(kbhit()){
-			lewo[0][0]=getch();
-			lewo[0][1]=getch();
-		}
-		else{
-			lewo[0][0]=getch();
-		}
+		}		
 		
 		int k=1;
 		int l=0;
-		while(true){
-			if(kbhit()){
-			lewo[0][0]=getch();
-			lewo[0][1]=getch();
-		}
-			else{
-				lewo[0][0]=getch();
-			}
-			
-			if(lewo[0][0]==sterowanie[0][0] && lewo[0][1]==sterowanie[0][1]){
-				plansza[SnakeX][SnakeY-l]=' ';
-				plansza[SnakeX][SnakeY-k]=snake;
-				k++;
-				l++;
-				system("cls");
-				for(int i=0;i<y;i++){
-					cout<<endl;
-					for(int j=0;j<x;j++){
-						cout<<plansza[i][j];
-					}
-				}
-			}
+		int m;
+		int klik[1][2]={};
+
+//		while(true){
+//			klik[0][0]=getch();
+//			if(kbhit()){
+//			klik[0][1]=getch();
+//			}	
+//			if(klik[0][0]==sterowanie[0][0] && klik[0][1]==sterowanie[0][1]){
+//				plansza[SnakeX][SnakeY-l]=' ';
+//				plansza[SnakeX][SnakeY-k]=snake;
+//
+//				}
+//				k++;
+//				l++;
+//				system("cls");
+//				for(int i=0;i<y;i++){
+//					cout<<endl;
+//					for(int j=0;j<x;j++){
+//						cout<<plansza[i][j];
+//					}
+//				}
+//			}
+//			 if(klik[0][0]==sterowanie[1][0] && klik[0][1]==sterowanie[1][1]){
+//				plansza[SnakeX][SnakeY+l]=' ';
+//				plansza[SnakeX][SnakeY+k]=snake;
+//				
+//				k++;
+//				l++;
+//				system("cls");
+//				for(int i=0;i<y;i++){
+//					cout<<endl;
+//					for(int j=0;j<x;j++){
+//						cout<<plansza[i][j];
+//					}
+//				}
+//			}
 		}
 		
 		return 0;
